9528c5fc7168f36a6b2923248d14e5e428b8fbe3
gh0stsh0t <rjperias0213@gmail.com>
2018-05-09 11:11:36 +0800

    changed to logging

--- proxpy.py
+++ proxpy.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 from random import randint
 import urllib2
-import re
 import requests
+import logging
+
 
 class proxyhandler:
     proxylist = []
@@ -12,10 +13,11 @@ class proxyhandler:
     def __init__(self):
         self.get_list()
         self.get_new()
+        self.get_proxy()
 
     def get_proxy(self):
         while True:
-            if self.test_domain() and (self.calls + randint(0,3)) < 15:
+            if self.test_domain() and (self.calls + randint(0, 3)) < 15:
                 self.calls += 1
                 return self.proxy
             else:
@@ -24,20 +26,18 @@ class proxyhandler:
     def get_new(self):
         while True:
             if self.proxylist:
-                while not self.test_domain():
-                    self.proxy = self.proxylist.pop()
-                    print(self.proxy)
-                    if not self.proxylist:
-                        self.get_list()
-                print('success?')
-                calls = 0
+                self.proxy = self.proxylist.pop()
+                logging.info('Retrieved New Proxy: '+self.proxy+'\n')
+                if not self.proxylist:
+                    self.get_list()
+                self.calls = 0
                 return
             else:
                 self.get_list()
 
     def get_list(self):
         page = urllib2.Request("https://www.us-proxy.org")
-        page.add_header('User-Agent','Mozilla/5.0 (X11; U; linux i686) Gecko/20071127 Firefox/2.0.0.11')
+        page.add_header('User-Agent', 'Mozilla/5.0 (X11; U; linux i686) Gecko/20071127 Firefox/2.0.0.11')
         opener = urllib2.build_opener()
         stream = opener.open(page).read()
         soup = BeautifulSoup(stream, "lxml")
@@ -46,21 +46,21 @@ class proxyhandler:
         for x in founds:
             found.append(x.text)
         scraped = []
-        for x in range(0,len(found)-31,8):
+        for x in range(0, len(found)-31, 8):
             if str(found[x+6]) == 'yes':
                 scraped.append("https://"+str(found[x])+":"+str(found[x+1]))
-        print(scraped)
+        logging.info(scraped)
         self.proxylist = scraped
 
     def test_domain(self):
-        protocol = 'https' 
-        test_url = '%s://%s' % (protocol, 'www.yellowpages.com')
-        print(self.proxy)
+        protocol = 'https'
+        test_url = '%s://%s' % (protocol, 'www.yell.com/')
+        print("testing: "+self.proxy)
         try:
-            r = requests.head(test_url, timeout=1, proxies={'https':self.proxy})
-            print(r.status_code)
-            print(test_url)
+            r = requests.head(test_url, timeout=1.5, proxies={'https': self.proxy})
+            logging.info(r.status_code)
+            logging.info(test_url)
             return True
         except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout) as e:
-            print("error")
+            logging.info("error\n")
             return False
