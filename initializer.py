d3523a119c578490386c9a9ae42586406d497886
gh0stsh0t <rjperias0213@gmail.com>
2018-05-23 08:59:59 +0800

    updated initializer/line endings

--- initializer.py
+++ initializer.py
@@ -1,4 +1,5 @@
 import pip._internal as pip
+pip.main(["install", "-r", "requirements.txt"])
 pip.main(["install", "scrapy", "docutils", "pygments"])
 pip.main(["install","--no-cache-dir","pypiwin32", "kivy.deps.sdl2", "kivy.deps.glew"])
 pip.main(["install", "kivy.deps.gstreamer"])
